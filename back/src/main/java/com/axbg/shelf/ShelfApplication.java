c51581e781355710eb311ba6b342ab0082c31bdd
axbg <bisagalexstefan@gmail.com>
2020-01-23 22:22:02 +0200

    Back Init

---
+++ back/src/main/java/com/axbg/shelf/ShelfApplication.java
@@ -0,0 +1,13 @@
+package com.axbg.shelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelfApplication.class, args);
+    }
+
+}
---
+++ back/src/main/java/com/axbg/shelf/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.axbg.shelf.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
