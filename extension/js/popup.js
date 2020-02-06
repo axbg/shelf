5acf71f963c9e4d3b9ef12fab55b6299b45acc52
Alex Bisag <bisagalexstefan@gmail.com>
2020-02-06 17:20:21 +0200

    Added spinner and a little bit of styling

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,9 +1,22 @@
+const toggleLoader = function(state){
+    if(state) {
+        document.getElementById("loader").style.display = "block";
+        document.getElementById("button").style.display = "none";
+    } else {
+        document.getElementById("loader").style.display = "none";
+        document.getElementById("button").style.display = "block";
+    }
+}
+
 document.getElementById("button").addEventListener("click", (event) => {
+    toggleLoader(true);
     chrome.cookies.getAll({}, (cookies) => {
         let concat = "";
         cookies.forEach(cookie => {
             concat += cookie.name + "\n";
         });
-        alert(concat);
+        setTimeout(() => {
+            toggleLoader(false);
+        }, 1000);
     })
 });
